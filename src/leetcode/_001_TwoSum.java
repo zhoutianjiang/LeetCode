024f39ba9a46a80abf4ff637667e828f95b03c4a
zhoutianjiang <892403353@qq.com>
2019-09-09 11:23:36 +0800

    1.Two Sum

---
+++ src/leetcode/_001_TwoSum.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class _001_TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length-1; i++) {
+			for (int j = i+1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					int x[] = {i, j};
+					return x;
+				}
+			}
+		}
+		throw new IllegalArgumentException("No such combination.");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nums[] = {2, 7, 11, 15};
+		int target = 9;
+		int s[] = twoSum(nums, target);
+		for (int i = 0; i < s.length; i++) {
+			System.out.print(s[i]);
+			if (i < s.length-1) {
+				System.out.print(",");
+			}
+		}
+	}
+
+}
