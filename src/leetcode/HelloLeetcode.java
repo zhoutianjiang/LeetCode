b85e8e3ff914623d2113e243a74c64a7e0e64af6
zhoutianjiang <892403353@qq.com>
2019-09-09 10:45:09 +0800

    Hello LeetCode

---
+++ src/leetcode/HelloLeetcode.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class HelloLeetcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello LeetCode");
+
+	}
+
+}
