051f12a0f01467b0de601ff65e05b6cfe632d8ea
zhoutianjiang <892403353@qq.com>
2019-09-09 14:08:06 +0800

    3.Longest Substring Without Repeating Characters

---
+++ src/leetcode/_003_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+public class _003_LongestSubstringWithoutRepeatingCharacters {
+	
+	public static int lengthOfLongestSubstring(String s) {
+		int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+        	int x = 0;
+        	String temp = "";
+        	for (int j = i; j < s.length(); j++) {
+        		char c = s.charAt(j);
+    			if (temp.contains(c+"") == false) {
+    				temp += c;
+    				x++;
+    			}
+    			else {
+					break;
+				}
+        	}
+			num = (x > num) ? x : num;
+		}
+		return num;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "qqqq";
+		
+		System.out.println(lengthOfLongestSubstring(s));
+	}
+
+}
