d5f97f4eeab1400003aadf91b86f468b54b0d00a
zhoutianjiang <892403353@qq.com>
2019-09-10 12:05:29 +0800

    6.ZigZag Conversion

---
+++ src/leetcode/_006_ZigZagConversion.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class _006_ZigZagConversion {
+	
+	public static String convert(String s, int numRows) {
+		if (numRows == 1) {
+			return s;
+		}
+		int m = numRows-1, n = s.length();
+		String c = "";
+        for (int r = 0; r <= m; r++) {
+			for (int i = 0; i < n; i++) {
+				if (i % (2 * m) == r || i % (2 * m) == 2 * m - r) {
+					c += s.charAt(i);
+				}
+			}
+		}
+		return c;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "PAYPALISHIRING";
+		int numRows = 3;
+		System.out.println(convert(s, numRows));
+	}
+
+}
