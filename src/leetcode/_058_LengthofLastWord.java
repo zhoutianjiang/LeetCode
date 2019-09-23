87abc5f6af6a4e51cb9fa9c3a021b48ac1e59556
ZhouTJ <ZhouTJ@DESKTOP-3OIST6R>
2019-09-23 11:09:30 +0800

    58. Length of Last Word

--- src/leetcode/_058_LengthofLastWord.java
+++ src/leetcode/_058_LengthofLastWord.java
@@ -4,33 +4,45 @@ public class _058_LengthofLastWord {
 	
 	public static int lengthOfLastWord(String s) {
 		
-		while (s.length() != 0) {
-			int i = s.lastIndexOf(' ');
-			if (i == s.length() - 1) {
-				s = s.substring(0, i);
-			}
-			else if (i == -1){
-				break;
-			}
-			else {
-				s = s.substring(i+1);
-				break;
-			}
+		int end = s.length() - 1;
+		while (end >= 0 && s.charAt(end) == ' ') {
+			end--;
 		}
-		
-		if (s.length() == 0) {
+		if (end < 0) {
 			return 0;
 		}
-		else {
-			return s.length();
+		int start = end;
+		while (start >= 0 && s.charAt(start) != ' ') {
+			start--;
 		}
+		return end - start;
+//		while (s.length() != 0) {
+//			int i = s.lastIndexOf(' ');
+//			if (i == s.length() - 1) {
+//				s = s.substring(0, i);
+//			}
+//			else if (i == -1){
+//				break;
+//			}
+//			else {
+//				s = s.substring(i+1);
+//				break;
+//			}
+//		}
+//		
+//		if (s.length() == 0) {
+//			return 0;
+//		}
+//		else {
+//			return s.length();
+//		}
 		
     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String s = "abc aaa dd ";
+		String s = "abc aaa  ddd  ";
 		System.out.println(lengthOfLastWord(s));
 		
 	}
