cb57054c4d2139d23bf707e03e3109481224dc70
ZhouTJ <ZhouTJ@DESKTOP-3OIST6R>
2019-09-25 17:01:40 +0800

    79. Word Search

--- src/leetcode/_079_WordSearch.java
+++ src/leetcode/_079_WordSearch.java
@@ -2,19 +2,19 @@ package leetcode;
 
 public class _079_WordSearch {
 	
-	public static char[][] bd;
-	public static String wd;
-	public static boolean[][] flag;
-	public static int[][] dircection = {{-1,0}, {0,-1}, {1,0},{0,1}};
+	public char[][] board;
+	public String word;
+	public boolean[][] flag;
+	public int[][] dircection = {{-1,0}, {0,-1}, {1,0},{0,1}};
 	
-	public static boolean exist(char[][] board, String word) {
-	
-		bd = board;
-		wd = word;
-		flag = new boolean[bd.length][bd[0].length];
+	public boolean exist(char[][] board, String word) {
 		
-		for (int i = 0; i < bd.length; i++) {
-			for (int j = 0; j < bd[0].length; j++) {
+		this.board = board;
+		this.word = word;
+		flag = new boolean[board.length][board[0].length];
+		
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
 				if (dfs(i, j, 0)) {
 					return true;
 				}
@@ -24,13 +24,13 @@ public class _079_WordSearch {
 		return false;
 	}
 	
-	public static boolean dfs(int i, int j, int start) {
+	public boolean dfs(int i, int j, int start) {
 		
-		if (start == wd.length() - 1) {
-			return bd[i][j] == wd.charAt(start);
+		if (start == word.length() - 1) {
+			return board[i][j] == word.charAt(start);
 		}
 		
-		if (bd[i][j] == wd.charAt(start)) {
+		if (board[i][j] == word.charAt(start)) {
 			flag[i][j] = true;
 			for (int k = 0; k < 4; k++) {
 				int newX = i + dircection[k][0];
@@ -47,9 +47,9 @@ public class _079_WordSearch {
 		return false;
 	}
 	
-	public static boolean inArea(int x, int y) {
+	public boolean inArea(int x, int y) {
 		
-		return x >=0 && x < bd.length && y >= 0 && y < bd[0].length;
+		return x >=0 && x < board.length && y >= 0 && y < board[0].length;
 	}
 	
 	public static void main(String[] args) {
@@ -58,7 +58,8 @@ public class _079_WordSearch {
 		char[][] board = {{'A','B','C','E'},
 						{'S','F','C','S'},
 						{'A','D','E','E'}};
-		System.out.println(exist(board, "ADEC"));
+		_079_WordSearch solution = new _079_WordSearch();
+		System.out.println(solution.exist(board, "ADEC"));
 		
 		
 	}
